3fbae6014d458459243d8fe61fc76d62332cf35f
MaKyuSeok <mks502@naver.com>
2019-10-01 14:32:37 +0900

    project INIT
    Question Rank server project INIT

---
+++ src/main/java/com/depromeet/qr/QuestionRankServerApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionRankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionRankServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/qr/QuestionRankServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.depromeet.qr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuestionRankServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
