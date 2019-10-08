a029aad28ebd0993bed2d45a57dbc2508699bc9d
MaKyuSeok <mks502@naver.com>
2019-10-08 10:43:53 +0900

    Comment , SeminarRoom Entity
    Comment , SeminarRoom Entity

---
+++ src/main/java/com/depromeet/qr/entity/Comment.java
@@ -0,0 +1,37 @@
+package com.depromeet.qr.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long commentId;
+
+	@Column(nullable = false)
+	private String content;
+	private String target;
+
+	@Builder.Default
+	private Integer likeCount = 0;
+
+	@ManyToOne
+	@JoinColumn(name = "seminarRoom")
+	private SeminarRoom seminarRoom;
+
+}
