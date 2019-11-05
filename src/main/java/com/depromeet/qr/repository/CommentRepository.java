a529a018c21ff7fb34fb623b439f462d252c8e98
MaKyuSeok <mks502@naver.com>
2019-11-05 19:44:22 +0900

    CommentService FIN
    댓글 서비스 완료
    댓글 CRUD

--- src/main/java/com/depromeet/qr/repository/CommentRepository.java
+++ src/main/java/com/depromeet/qr/repository/CommentRepository.java
@@ -1,10 +1,14 @@
 package com.depromeet.qr.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.depromeet.qr.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 	public Comment findOneBycommentId(Long commentId);
-	
+
+	public List<Comment> findAllBySeminarRoom(Long seminarId);
+
 }
