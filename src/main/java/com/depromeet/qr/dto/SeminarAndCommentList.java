c323df4325a8c05cbd54caceb3c55df919ebea76
MaKyuSeok <mks502@naver.com>
2020-11-04 17:08:11 +0900

    Webconfig Modify

--- src/main/java/com/depromeet/qr/dto/SeminarAndCommentList.java
+++ src/main/java/com/depromeet/qr/dto/SeminarAndCommentList.java
@@ -5,12 +5,13 @@ import java.util.List;
 import com.depromeet.qr.entity.Comment;
 import com.depromeet.qr.entity.Member;
 
+import com.depromeet.qr.entity.SeminarRoom;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class MemberAndCommentList {
-	public Member member;
+public class SeminarAndCommentList {
+	public SeminarRoom seminarRoom;
 	public List<SpeakerAndCommentList> commentListBySpeaker;
 }
